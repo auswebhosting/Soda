e86cc614fd9f59290fb0b11801480558564626cc
Drew <drewsy.morris@gmail.com>
2014-07-22 12:42:44 +1000

    Added signature / test mode support

--- src/Soda/Provider/Omnipay/OmnipayServiceProvider.php
+++ src/Soda/Provider/Omnipay/OmnipayServiceProvider.php
@@ -19,5 +19,7 @@ class OmnipayServiceProvider implements ServiceProviderInterface
     	$app['omnipay.factory'] = Omnipay::create($app['omnipay.config']['gateway']);
     	$app['omnipay.factory']->setUsername($app['omnipay.config']['username']);
     	$app['omnipay.factory']->setUsername($app['omnipay.config']['password']);
+    	$app['omnipay.factory']->setSignature($app['omnipay.config']['signature']);
+    	$app['omnipay.factory']->setTestMode($app['omnipay.config']['testing']);
     }
 }
