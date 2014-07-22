c6933efb49f069fcb0eb822519a8622b537bb5a8
Drew <drewsy.morris@gmail.com>
2014-07-22 13:24:20 +1000

    Omnipay now cycles through config attributes.

---
+++ src/Soda/Provider - Copy/Omnipay/OmnipayServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Soda\Provider\Omnipay;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+
+use Omnipay\Common\CreditCard;
+use Omnipay\Omnipay;
+
+class OmnipayServiceProvider implements ServiceProviderInterface
+{
+    public function register(Application $app)
+    {
+    }
+
+    public function boot(Application $app)
+    {
+    	$app['omnipay.factory'] = Omnipay::create($app['omnipay.config']['gateway']);
+    	$app['omnipay.factory']->setUsername($app['omnipay.config']['username']);
+    	$app['omnipay.factory']->setPassword($app['omnipay.config']['password']);
+    	$app['omnipay.factory']->setSignature($app['omnipay.config']['signature']);
+    	$app['omnipay.factory']->setTestMode($app['omnipay.config']['testing']);
+    }
+}
--- src/Soda/Provider/Omnipay/OmnipayServiceProvider.php
+++ src/Soda/Provider/Omnipay/OmnipayServiceProvider.php
@@ -16,10 +16,14 @@ class OmnipayServiceProvider implements ServiceProviderInterface
 
     public function boot(Application $app)
     {
-    	$app['omnipay.factory'] = Omnipay::create($app['omnipay.config']['gateway']);
-    	$app['omnipay.factory']->setUsername($app['omnipay.config']['username']);
-    	$app['omnipay.factory']->setPassword($app['omnipay.config']['password']);
-    	$app['omnipay.factory']->setSignature($app['omnipay.config']['signature']);
-    	$app['omnipay.factory']->setTestMode($app['omnipay.config']['testing']);
+        foreach($app['omnipay.config'] as $key => $value)
+        {
+            $app['omnipay.factory.' . $key] = Omnipay::create($value['gateway']);
+
+            foreach ($value as $attribute => $setting)
+            {
+                call_user_func(array($app['omnipay.factory.' . $key], 'set' . ucfirst($attribute)), $setting);
+            }
+        }
     }
 }
